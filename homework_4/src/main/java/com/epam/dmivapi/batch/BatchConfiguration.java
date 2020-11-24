449e49d81c4e68cbbcb3d75f1e7ba5ed7ee48fa4
Dmytro Pishchanetskyi <Dmytro_Pishchanetskyi@epam.com>
2020-11-24 23:58:12 +0200

    Batch processing

---
+++ homework_4/src/main/java/com/epam/dmivapi/batch/BatchConfiguration.java
@@ -0,0 +1,32 @@
+package com.epam.dmivapi.batch;
+
+import org.springframework.context.annotation.Bean;
+
+public class BatchConfiguration {
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names(new String[]{"firstName", "lastName"})
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                    setTargetType(Person.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+                .dataSource(dataSource)
+                .build();
+    }
+}
