8b7cb15fb5e3d6dda4d850853a07e05db0a60e50
Dmytro Pishchanetskyi <Dmytro_Pishchanetskyi@epam.com>
2020-11-27 03:13:23 +0200

    custom healthCheck

---
+++ homework_5/src/main/java/com/epam/dmivapi/springbootdemo/AppNameHealthIndicator.java
@@ -0,0 +1,13 @@
+package com.epam.dmivapi.springbootdemo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppNameHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.up().withDetail("Application name", "homework_5").build();
+    }
+}
